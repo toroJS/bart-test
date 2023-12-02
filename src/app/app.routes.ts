a018289db184e43065a438d467f11d7ff4f5d30a
Daniel Toro <torodhtoro@gmail.com>
2023-12-02 02:10:18 +0100

    add route for game, template cleanup, game component and service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',   redirectTo: '/game', pathMatch: 'full' }, 
+    {
+        path: 'game',
+        component: GameComponent
+    }
+];
