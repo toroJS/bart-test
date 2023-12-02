d89d496979ef8d080ef655383b2dfdba47dea4c4
Daniel Toro <torodhtoro@gmail.com>
2023-12-02 22:42:25 +0100

    show snack when collecting points

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { BalloonComponent } from '../balloon/balloon.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-game',
@@ -19,16 +21,25 @@ export class GameComponent {
   totalPoints$ = this.gameService.totalPoints$;
   burts$ = this.gameService.burst$;
 
-  constructor(private gameService: GameService) {}
+  constructor(
+    private gameService: GameService,
+    private _snackBar: MatSnackBar
+  ) {}
 
   pumpBalloon() {
     this.gameService.pumpBalloon();
   }
 
   collectPoints() {
-    alert('points collected');
-
+    this.openSnackBar('Points collected', '🎉🎉🎉');
     this.gameService.collectPoints();
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+      verticalPosition: 'top',
+    });
   }
 
   nextRound() {
