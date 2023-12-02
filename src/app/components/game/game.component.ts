cfebd32fbdf5f0fe8c766633fcd22dcd9dbc4a3d
Daniel Toro <torodhtoro@gmail.com>
2023-12-02 02:22:01 +0100

    game logic

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { GameService } from './game.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
+  balloonHeight$ = this.gameService.balloonHeight$;
+  balloonWidth$ = this.gameService.balloonWidth$;
+  burts$ = this.gameService.burst$;
 
+  constructor(private gameService: GameService) {}
+
+  pumpBalloon() {
+    const pumpStrength = Math.random();
+    this.gameService.pumpBalloon(pumpStrength);
+  }
 }
--- src/app/components/game/game.service.ts
+++ src/app/components/game/game.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameService {
+  private balloonHeightSubject = new BehaviorSubject<string>('100px');
+  balloonHeight$ = this.balloonHeightSubject.asObservable();
+  private balloonWidthSubject = new BehaviorSubject<string>('100px');
+  balloonWidth$ = this.balloonWidthSubject.asObservable();
+  private burstSubject = new BehaviorSubject<boolean>(false);
+  burst$ = this.burstSubject.asObservable();
 
-  constructor() { }
+  pumpBalloon(pumpStrength: number) {
+    const burstThreshold = 0.8;
+
+    if (
+      !this.burstSubject.value &&
+      Math.random() < burstThreshold * pumpStrength
+    ) {
+      this.burstSubject.next(true);
+    } else {
+      this.balloonHeightSubject.next(
+        this.calculateNewSize(this.balloonHeightSubject.value, pumpStrength)
+      );
+      this.balloonWidthSubject.next(
+        this.calculateNewSize(this.balloonWidthSubject.value, pumpStrength)
+      );
+    }
+  }
+
+  resetBalloon() {
+    this.burstSubject.next(false);
+    this.balloonHeightSubject.next('100px');
+    this.balloonWidthSubject.next('100px');
+  }
+
+  private calculateNewSize(currentSize: string, pumpStrength: number): string {
+    return parseInt(currentSize) + pumpStrength * 10 + 'px';
+  }
 }
