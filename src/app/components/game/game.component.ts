264877a73dcfe2f763a776b5105193d567ab2049
Daniel Toro <torodhtoro@gmail.com>
2023-12-03 01:45:58 +0100

    add debounce

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { GameService } from './game.service';
 import { CommonModule } from '@angular/common';
 import { BalloonComponent } from '../balloon/balloon.component';
@@ -6,6 +6,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
+import { Subject, debounceTime, map, takeUntil, tap } from 'rxjs';
+
+enum ACTION {
+  PUMP = 'pump',
+  COLLECT = 'collect',
+  NEXT_ROUND = 'next_round',
+}
 
 @Component({
   selector: 'app-game',
@@ -21,24 +28,74 @@ import { MatCardModule } from '@angular/material/card';
     MatCardModule,
   ],
 })
-export class GameComponent {
+export class GameComponent implements OnDestroy {
+  private destroy$ = new Subject<void>();
+  private clickSubject = new Subject<ACTION>();
+  public ACTIONTYPE = ACTION;
+  public disable = false;
+
   round$ = this.gameService.round$;
+  progress$ = this.gameService.round$.pipe(
+    map((round) => {
+      const totalRounds = this.gameService.maxRound;
+      const porcentage = (round * 100) / totalRounds;
+      return porcentage;
+    })
+  );
   gameEnd$ = this.gameService.gameEnd$;
   balloonSize$ = this.gameService.balloonSize$;
   totalPoints$ = this.gameService.totalPoints$;
   burts$ = this.gameService.burst$;
+  username = this.gameService.user;
 
   constructor(private gameService: GameService) {}
 
-  pumpBalloon() {
+  ngOnInit() {
+    this.clickSubject
+      .pipe(
+        tap(() => (this.disable = true)),
+        debounceTime(200)
+      )
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((action: ACTION) => {
+        switch (action) {
+          case ACTION.PUMP:
+            this.pumpBalloon();
+            break;
+
+          case ACTION.COLLECT:
+            this.collectPoints();
+            break;
+
+          case ACTION.NEXT_ROUND:
+            this.nextRound();
+            break;
+
+          default:
+            break;
+        }
+        this.disable = false;
+      });
+  }
+
+  onButtonClick(action: ACTION) {
+    this.clickSubject.next(action);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private pumpBalloon() {
     this.gameService.pumpBalloon();
   }
 
-  collectPoints() {
+  private collectPoints() {
     this.gameService.collectPoints();
   }
 
-  nextRound() {
+  private nextRound() {
     this.gameService.nextRound();
   }
 }
